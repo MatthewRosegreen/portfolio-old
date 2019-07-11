9b8190d8830c70498ad724d101102d9d3b4ce4b9
MatthewRosegreen <33874299+MatthewRosegreen@users.noreply.github.com>
2019-07-11 02:23:27 +0100

    csv into observable values

--- projects.js
+++ projects.js
@@ -30,21 +30,21 @@ var GetCSV = function(){
 		resolve(
 			d3.dsv(",", "https://matthewrosegreen.github.io/portfolio/projectsdata.csv", function(d){
 				return{
-					name: d.csvName,
-					role: d.csvRole,
-					dateCreated: d.csvDateCreated,
-					createdWith: d.csvCreatedWith,
-					descOpener: d.csvOpener,
-					description: d.csvDescription,
-					intent: d.csvIntent,
-					imgType: d.csvImgType,
-					imgBase64: d.csvImgBase64,
-					linkDesc1: d.csvLinkDesc1,
-					linkUrl1: d.csvLinkUrl1,
-					linkDesc2: d.csvLinkDesc2,
-					linkUrl2: d.csvLinkUrl2,
-					linkDesc3: d.csvLinkDesc3,
-					linkUrl3: d.csvLinkUrl3
+					name: ko.observable(d.csvName),
+					role: ko.observable(d.csvRole),
+					dateCreated: ko.observable(d.csvDateCreated),
+					createdWith: ko.observable(d.csvCreatedWith),
+					descOpener: ko.observable(d.csvOpener),
+					description: ko.observable(d.csvDescription),
+					intent: ko.observable(d.csvIntent),
+					imgType: ko.observable(d.csvImgType),
+					imgBase64: ko.observable(d.csvImgBase64),
+					linkDesc1: ko.observable(d.csvLinkDesc1),
+					linkUrl1: ko.observable(d.csvLinkUrl1),
+					linkDesc2: ko.observable(d.csvLinkDesc2),
+					linkUrl2: ko.observable(d.csvLinkUrl2),
+					linkDesc3: ko.observable(d.csvLinkDesc3),
+					linkUrl3: ko.observable(d.csvLinkUrl3)
 				};
 			})
 		);
